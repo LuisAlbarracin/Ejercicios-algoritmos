fba04afaba151b36f0efc94c726dde6f2437d182
Luis Vicente Albarracin <luisalbarracin9727@gmail.com>
2022-09-10 17:28:06 -0500

    fizzbuzz

---
+++ src/co/edu/ufps/sistemas/algoritmos/ejercicios/GFG.java
@@ -0,0 +1,35 @@
+package co.edu.ufps.sistemas.algoritmos.ejercicios;
+
+public class GFG {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s = "";
+		int c3 = 0, c5 = 0;
+		
+		for(int i = 1; i <= 100; i++) {
+			
+			c3++;
+			c5++;
+			
+			if(c3 == 3) {
+				s += "fizz";
+				c3 = 0;
+			}
+			if (c5 == 5) {
+				s += "buzz";
+				c5 = 0;
+			}
+			if(s.length() == 0)
+				System.out.println(i);
+			else
+				System.out.println(s);
+			
+			s = "";
+		}
+		
+		
+	}
+
+}
